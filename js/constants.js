c509d07ed20d2c5afe03eea3ea1528643f17ee27
Nialyn Mid <nialyn.mid@gmail.com>
2026-05-03 16:35:01 -0500

    compact mode fix

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 export const MODULE_NAME = 'polyceph';
-export const VERSION = '0.8.3';
+export const VERSION = '0.8.4';
 export const PIPELINE_DATA_VERSION = '1.0.0';
 
 export const defaultSettings = {
